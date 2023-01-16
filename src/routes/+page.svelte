63aad5378fb3804c95b9fa3cbaca86dcfd30c85a
Ahmad Shiddiq Nurrahman <asn1998@gmail.com>
2023-01-17 00:22:38 +0700

    fix bug, enhance from submission

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { onDestroy } from "svelte";
+	import { onDestroy, onMount } from "svelte";
     import { enhance } from '$app/forms';
     import pb from "$db/pocketbase";
 	import type { ActionData, PageData } from "./$types";
@@ -9,6 +9,9 @@
     export let form: ActionData
 
     let filter: Topic
+    let question: string
+    let disabled = false
+    let formEle: HTMLFormElement
     let descriptions = [
         "Pertanyaan tentang pribadi seseorang",
         "Pertanyaan tentang masa-masa muda. Bisa pas SMA, DKM, atau mungkin lebih muda lagi (KB, TK, SD, SMP), atau sedikit lebih tua (Kuliah)",
@@ -22,8 +25,8 @@
     $: shownQuestions = questions.filter(i => i.topic == filter)
 
     // onMount(async () => {
-    //     await pb.collection('users').authWithPassword("asn1998", "123456789")
-
+    //     filter = filter
+    //     console.log(filter)
     // })
 
     $: {
@@ -70,21 +73,26 @@
                     </ul>
                     <h6>Tambah Pertanyaan</h6>
                     <form 
+                        bind:this={formEle}
                         method="post"
                         action="?/submit"
                         use:enhance={() => {
                             // prevent default callback from resetting the form
-                            return ({ update }) => {
-                                update({ reset: true });
+                            return ({ update, result }) => {
+                                update({ reset: false });
+                                if (result.type == "success") {
+                                    question = ""
+                                }
+                                disabled = false
                             };
                         }}
                     
                     >
-                        <input class="form-control mb-2" minlength="9" type="text" name="question" required>
+                        <input class="form-control mb-2" minlength="9" type="text" name="question" bind:value={question} required {disabled}>
                         <input class="d-none" name="topic" bind:value={filter}>
                         <div class="d-flex justify-content-end">
-                            <button class="btn btn-primary me-1" type="submit">Tambah</button>
-                            <button class="btn btn-secondary" type="reset">Reset</button>
+                            <button class="btn btn-primary me-1" type="submit" on:click={() => {disabled = true; formEle.requestSubmit()}} {disabled}>Tambah</button>
+                            <button class="btn btn-secondary" type="reset" {disabled}>Reset</button>
                         </div>
                     </form>
                 </div>
